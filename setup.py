90b45ebdde7430ec153c26e5e21d3c4472e04e8f
Syn-dromatic <68112904+Syn-dromatic@users.noreply.github.com>
2023-02-11 04:12:21 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
+from pathlib import Path
 from setuptools import setup, find_packages
 
 VERSION = "1.0.0"
 DESCRIPTION = "Timer Module with profiling features"
+
+root = Path(__file__).parent
+long_description = (root / "README.md").read_text()
 
 setup(
     name="timer-module",
@@ -10,6 +14,8 @@ setup(
     url="https://github.com/syn-chromatic",
     version=VERSION,
     description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(exclude=["examples"]),
     python_requires=">=3.10",
     install_requires=["setuptools>=45.0"],
